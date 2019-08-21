a62205816eb362a026ec50ba3f8c6c0904627dab
ciny <stanislav.marcinko@satoshilabs.com>
2019-08-21 15:09:29 +0200

    core: removed unused function from storage.recovery_shares

--- core/src/apps/common/storage/recovery_shares.py
+++ core/src/apps/common/storage/recovery_shares.py
@@ -20,13 +20,6 @@ def get(index: int) -> Optional[str]:
     return None
 
 
-def get_index_from_group(index: int, group_index: int) -> Optional[str]:
-    m = common._get(common._APP_RECOVERY_SHARES, index + (group_index - 1) * 16)
-    if m:
-        return m.decode()
-    return None
-
-
 def fetch(group_count: int = 1) -> List[str]:
     mnemonics = []
     for index in range(0, slip39.MAX_SHARE_COUNT * group_count):
